387c1f732668f3dfe565d5bd4063f0fad6b8b1b0
Romain Ferrali <rferrali@gmail.com>
2024-11-14 02:11:45 +0100

    blueprint create

--- R/create.R
+++ R/create.R
@@ -1,10 +1,15 @@
-create <- function(name, remote, local) {
+create <- function(name, remote = NULL, local = NULL) {
   print("creating")
   # error
   ## name exists
   # TODO
-  # add remote to .env
+  # remote
   ## default remote is remote_default/name
+  if(is.null(remote)) {
+    # TODO
+  }
+  ## error if remote doesn't exist
+  # add remote to .env
   # add project to lockfile
   ## default local is local_default/name
 }
--- R/functions.R
+++ R/functions.R
@@ -3,13 +3,21 @@ read_config <- function() {
   config <- jsonlite::read_json("./gitlatex.lock")
   # check schema
   # TODO
-  # augment projects
+  # augment config with secrets
+  config$remote_default <- Sys.getenv("GITLATEX_REMOTE_DEFAULT")
+  # augment and check projects 
   config$projects <- lapply(config$projects, function(project) {
+    # augment with secrets and local path info
     project$remote <- Sys.getenv(sprintf("GITLATEX_PROJECT_%s", project$name))
     project$remote_normalized <- normalizePath(project$remote)
     project$remote_parent <- normalizePath(sprintf("%s/..", project$remote_normalized))
     project$local_normalized <- normalizePath(project$local)
     project$local_parent <- normalizePath(sprintf("%s/..", project$local_normalized))
+    # check
+      ## check existence of project paths
+    # TODO
+      ## in local, create symlinks to assets if missing
+    # TODO
     return(project)
   })
   return(config)
