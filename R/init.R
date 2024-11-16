0b49ba9c11e81fb2799a2a0713aa6caab327d31b
Romain Ferrali <rferrali@gmail.com>
2024-11-16 01:45:24 +0100

    bugfix

--- R/init.R
+++ R/init.R
@@ -25,24 +25,28 @@ init <- function(
     "public" = file.exists(public),
     "private" = file.exists(private),
     "assets" = dir.exists(assets)
-
   )
-  if(any(exists)) {
+  if(any(exists[-"assets"])) {
     stop("gitlatex has already been initialized")
   }
-  cli::cli_bullets(c(
+  msg <- c(
     "i" = "The following files will be created:",
     "*" = glue::glue("Public config: {public}"),
-    "*" = glue::glue("Private config: {private}"),
-    "*" = glue::glue("Assets directory: {assets}")
-  ))
+    "*" = glue::glue("Private config: {private}")
+  )
+  if(!exists["assets"]) {
+    msg <- c(msg, "*" = glue::glue("Assets directory: {assets}"))
+  }
+  cli::cli_bullets(msg)
   choice <- utils::menu(c("Yes", "No"), title = "Initialize gitlatex?")
   if(choice == 1) {
     cfg <- .CONFIG_EXAMPLE
     cfg$assets <- assets
     jsonlite::write_json(cfg, public, auto_unbox = TRUE, pretty = TRUE)
     jsonlite::write_json(.CONFIG_PRIVATE_EXAMPLE, private, auto_unbox = TRUE, pretty = TRUE)
-    dir.create(assets)
+    if(!exists("assets")) {
+      dir.create(assets)
+    }
     cli::cli_alert_success("gitlatex initialized")
   } else {
     cli::cli_alert_danger("gitlatex initialization cancelled")
